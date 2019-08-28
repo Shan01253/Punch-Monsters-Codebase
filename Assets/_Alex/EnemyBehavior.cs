3e5a0cbbcf3d87af5c1689d59877536077ea71f2
BEPISMAN2 <helpithinkihavehermes@gmail.com>
2019-08-28 14:50:53 -0400

    basic state machine setup and testing

---
+++ Assets/_Alex/EnemyBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlexCode {
+
+public class EnemyBehavior : MonoBehaviour
+{
+	public GameObject target;	
+
+	private StateMachine enStateMachine;
+	private Rigidbody2D rb;
+
+
+	public void testCallback() {
+		Debug.Log("Callback reached.");
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rb = gameObject.GetComponent<Rigidbody2D>();
+
+		enStateMachine = new StateMachine();
+		enStateMachine.ChangeTo(new EnAI_Approach(target, rb, testCallback)); 
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		enStateMachine.Execute();
+	}
+}
+
+}
